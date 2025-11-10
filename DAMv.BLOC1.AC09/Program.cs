76e7efcf0a3ce4d31e743701b985774b24512f84
miquel-manzano <miquel.manzano.7e6@itb.cat>
2025-11-10 14:00:43 +0100

    Exercise three done, ToDo: refactoring.

--- DAMv.BLOC1.AC09/Program.cs
+++ DAMv.BLOC1.AC09/Program.cs
@@ -6,15 +6,25 @@
         Random rnd = new Random();
 
         // Fill the array with random numbers between 1 and 10
+        int index = 0;
         foreach (int number in tenNumbers)
         {
-            tenNumbers[number] = rnd.Next(1, 10);
+            tenNumbers[index] = rnd.Next(1, 10);
+            index++;
         }
 
+        Console.WriteLine("Ten numbers on order");
         // Print the numbers
-        foreach (int number in tenNumbers)
+        for (int i = 0; i < tenNumbers.GetLength(0); i++)
         {
-            Console.WriteLine(number);
+            Console.Write($"{tenNumbers[i]} ");
+        }
+
+        Console.WriteLine("Ten numbers on inverse order");
+        // Print the numbers in reverse order
+        for (int i = tenNumbers.GetLength(0)-1; i >= 0; i--)
+        {
+            Console.Write($"{tenNumbers[i]} ");
         }
     }
 }
