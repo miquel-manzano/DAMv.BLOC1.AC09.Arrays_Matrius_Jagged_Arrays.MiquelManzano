592f2094dff1a669bd7d1f93b3ff395843f2cdfb
miquel-manzano <miquel.manzano.7e6@itb.cat>
2025-11-10 13:50:55 +0100

    Not done..

--- DAMv.BLOC1.AC09/Program.cs
+++ DAMv.BLOC1.AC09/Program.cs
@@ -2,6 +2,19 @@
 {
     public static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        int[] tenNumbers = new int[10];
+        Random rnd = new Random();
+
+        // Fill the array with random numbers between 1 and 10
+        foreach (int number in tenNumbers)
+        {
+            tenNumbers[number] = rnd.Next(1, 10);
+        }
+
+        // Print the numbers
+        foreach (int number in tenNumbers)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
