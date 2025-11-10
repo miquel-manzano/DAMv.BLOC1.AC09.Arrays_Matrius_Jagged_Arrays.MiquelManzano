7f9e4d0df69f7cb7d67f11b61da3e8be252e808f
miquel-manzano <miquel.manzano.7e6@itb.cat>
2025-11-10 13:15:32 +0100

    Exercise one finished.

--- DAMv.BLOC1.AC09/Program.cs
+++ DAMv.BLOC1.AC09/Program.cs
@@ -2,6 +2,23 @@
 {
     public static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        //Exercise One
+        int[] twientyNumbers = new int[20];
+        Random rnd = new Random();
+
+        // Fill the array with random numbers
+        int index = 0;
+        foreach (int number in twientyNumbers)
+        {
+            twientyNumbers[index] = rnd.Next(1, 101);
+            index++;
+        }
+
+        // Print the array
+        Console.WriteLine($"Your array with length {twientyNumbers.GetLength(0)}:");
+        foreach (int number in twientyNumbers)
+        {
+            Console.Write($"{number} ");
+        }
     }
 }
