8ee7c5f5e41f15628540552d156a611218a06d8d
miquel-manzano <miquel.manzano.7e6@itb.cat>
2025-11-11 10:24:32 +0100

    Not done

--- DAMv.BLOC1.AC09/Program.cs
+++ DAMv.BLOC1.AC09/Program.cs
@@ -2,32 +2,16 @@
 {
     public static void Main()
     {
-        const string OrderNumMsg = "Ten numbers on order";
-        const string InverseOrderNumMsg = "Ten numbers on inverse order";
-
         int[] tenNumbers = new int[10];
+        int[] orderedTenNumbers = new int[tenNumbers.GetLength(0)];
         Random rnd = new Random();
 
-        // Fill the array with random numbers between 1 and 10
-        int index = 0;
-        foreach (int number in tenNumbers)
-        {
-            tenNumbers[index] = rnd.Next(1, 10);
-            index++;
-        }
-
-        Console.WriteLine(OrderNumMsg);
-        // Print the numbers
+        
         for (int i = 0; i < tenNumbers.GetLength(0); i++)
         {
-            Console.Write($"{tenNumbers[i]} ");
+            tenNumbers[i] = rnd.Next(1, 10);
         }
 
-        Console.WriteLine(InverseOrderNumMsg);
-        // Print the numbers in reverse order
-        for (int i = tenNumbers.GetLength(0)-1; i >= 0; i--)
-        {
-            Console.Write($"{tenNumbers[i]} ");
-        }
+        
     }
 }
