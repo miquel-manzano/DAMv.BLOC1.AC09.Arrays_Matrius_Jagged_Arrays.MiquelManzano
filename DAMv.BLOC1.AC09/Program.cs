7aada76a99d0f22bbfa206c317341e5d81453791
miquel-manzano <miquel.manzano.7e6@itb.cat>
2025-11-11 10:33:19 +0100

    Exercise one done.

--- DAMv.BLOC1.AC09/Program.cs
+++ DAMv.BLOC1.AC09/Program.cs
@@ -3,6 +3,8 @@
     public static void Main()
     {
         const string ArrayLengthMsg = "Your array with length {0}:";
+        const string EvenNumbersMsg = "\nEven numbers in the array:";
+        const string OddNumbersMsg = "\nOdd numbers in the array:";
 
         int[] twientyNumbers = new int[20];
         Random rnd = new Random();
@@ -20,6 +22,25 @@
         foreach (int number in twientyNumbers)
         {
             Console.Write($"{number} ");
+        }
+
+        // Print even and odd numbers
+        Console.WriteLine(EvenNumbersMsg);
+        foreach (int number in twientyNumbers)
+        {
+            if (number % 2 == 0)
+            {
+                Console.Write($"{number} ");
+            }
+        }
+
+        Console.WriteLine(OddNumbersMsg);
+        foreach (int number in twientyNumbers)
+        {
+            if (number % 2 != 0)
+            {
+                Console.Write($"{number} ");
+            }
         }
     }
 }
