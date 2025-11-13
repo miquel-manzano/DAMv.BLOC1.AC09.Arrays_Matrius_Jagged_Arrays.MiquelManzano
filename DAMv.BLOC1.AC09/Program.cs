99929d0e7d192095ad9ea38ed3e483f89e7f0e48
miquel-manzano <miquel.manzano.7e6@itb.cat>
2025-11-13 12:19:54 +0100

    Exercise eight done.

--- DAMv.BLOC1.AC09/Program.cs
+++ DAMv.BLOC1.AC09/Program.cs
@@ -2,6 +2,21 @@
 {
     public static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        string[] Players = { "ShadowWolf", "PixelNinja", "CyberMage", "DragonSlayer" };
+        int[,] Matches = new int[4,4];
+
+        Random rnd = new Random();
+
+        Console.WriteLine("Player   -   Match1 - Match2 - Match3 - Match4 - Match5");
+        for (int i = 0; i < Matches.GetLength(0); i++)
+        {
+            Console.Write($"{Players[i]}: ");
+            for (int j = 0; j < Matches.GetLength(1); j++)
+            {
+                Matches[i,j] = rnd.Next(0, 11);
+                Console.Write($"{Matches[i,j]} ");
+            }
+            Console.WriteLine();
+        }
     }
 }
